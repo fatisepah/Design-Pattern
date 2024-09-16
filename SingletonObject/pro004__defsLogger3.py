f82e2e6ea560f24a59db55852a8d90505c435115
Fatemeh Sepahvand <51885589+fatisepah@users.noreply.github.com>
2024-09-16 19:59:36 +0330

    delete some files

--- SingletonObject/pro004__defsLogger3.py
+++
@@ -1,6 +0,0 @@
-import pro002__defsLogger1
-
-try:
-    a=1/0
-except:
-    pro002__defsLogger1.error("divide by zero error")
--- SingletonObject/pro007__classLogger2.py
+++
@@ -1,4 +0,0 @@
-from pro006__classLogger1 import Logger
-
-logger_obj=Logger("logi.txt")
-logger_obj.info("Info message")
